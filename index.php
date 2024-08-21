3029af13d6ce5e346a11c6aa9569ca46aaef1767
Loopaman <allahisrazaaq@gmail.com>
2024-08-21 17:05:06 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thirty Minutes</title>
+    <link rel="stylesheet" href="external-resources/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    
+</head>
+<body>
+    <!--Add New Exercise-->
+<div class="card" style="width: 18rem;">
+    <button id="addNewExercise" type="button" class="btn btn-success">Add New Exercise</button>
+  <div class="card-body">
+    
+   <!-- add exercises here-->
+  </div>
+</div>
+
+    <!-- Link to JavaScript file -->
+    <script src="external-resources/bootstrap-5.3.3-dist/js/bootstrap.min.js"> </script>
+    <script src="js/add_new_exercise.js"> </script>
+</body>
+</html>
